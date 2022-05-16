e97f3262846866df1c12a83f627461397f76b7ed
Sylwesterl5 <68221473+Sylwesterl5@users.noreply.github.com>
2022-05-16 22:49:23 +0200

    finish

---
+++ index.js
@@ -0,0 +1,7 @@
+const YEAR = 2100;
+
+if((YEAR % 4 === 0 && YEAR % 100 !== 0) || YEAR % 400 === 0){
+    console.log(`Rok ${YEAR} jest rokiem przestępnym`);
+}else{
+    console.log(`Rok ${YEAR} nie jest rokiem przestępnym`);
+}
